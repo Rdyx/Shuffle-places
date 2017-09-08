1fd753da160aab9b3020221b92516da2007089b7
Rdyx <tiranico.pro@gmail.com>
2017-09-08 16:48:39 +0200

    finish

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-$('#shuffle').click(function () {
-    for (var i = 0; i < 15; i++) {
-        $('#' + i + '').html(i);
-    }
-});
+var array = [];
 
 $.ajax({
-    url: "https://spreadsheets.google.com/feeds/list/1-oYlvGP573O4ml4AzgapLhK_KoEqlfDIzvSnWz48_nQ/od6/public/basic?alt=json",
-    //first row "title" column
+    url: "https://spreadsheets.google.com/feeds/list/YOUR_KEY/od6/public/values?alt=json",
     success: function (data) {
         console.log(data);
         for (var i = 0; i < 15; i++) {
-            $('#' + i + '').html(data.feed.entry[i].content.$t);
+            array.push([data.feed.entry[i].title.$t,data.feed.entry[i].gsx$url.$t]);
+            $('#table' + i + '').html(array[i][0] + '<br><img src="'+array[i][1]+'">');
         }
-        //console.log(data.feed.entry[0]['gsx$title']['$t']);
+        console.log(array)
+    }
+})
+
+$('#shuffle').click(function () {
+    var array2 = shuffle(array);
+    for (var i = 0; i < 15; i++) {
+        $('#table' + i + '').html(array2[i][0] + '<br><img src="'+array2[i][1]+'">');
+    }
+});
+
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // Tant qu'il reste des éléments du tableau non shuffle
+    while (0 !== currentIndex) {
+
+        //Sélectionne un élément restant
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // Et swap avec l'élément actuel
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-})
+    return array;
+}
+
