a5a358062957ed6520a6682b7beb1b86406eef49
Rdyx <tiranico.pro@gmail.com>
2017-09-08 15:21:47 +0200

    ajax ok

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-$('#shuffle').click(function (){
-    for(var i = 0; i < 15; i++){
-        $('#'+i+'').html(i);
+$('#shuffle').click(function () {
+    for (var i = 0; i < 15; i++) {
+        $('#' + i + '').html(i);
     }
+});
+
+$.ajax({
+    url: "https://spreadsheets.google.com/feeds/list/1-oYlvGP573O4ml4AzgapLhK_KoEqlfDIzvSnWz48_nQ/od6/public/basic?alt=json",
+    //first row "title" column
+    success: function (data) {
+        console.log(data);
+        for (var i = 0; i < 15; i++) {
+            $('#' + i + '').html(data.feed.entry[i].content.$t);
+        }
+        //console.log(data.feed.entry[0]['gsx$title']['$t']);
+    }
+
 })
